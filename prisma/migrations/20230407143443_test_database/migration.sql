6863ef59c77deca7288cec46094392d49756b12b
Damsbot64 <damien64durand@gmail.com>
2023-04-07 16:39:55 +0200

    added connection to DB

---
+++ prisma/migrations/20230407143443_test_database/migration.sql
@@ -0,0 +1,20 @@
+-- CreateTable
+CREATE TABLE "Example" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Example_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "test" (
+    "id" TEXT NOT NULL,
+    "name" TEXT,
+    "content" TEXT,
+    "image" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "test_pkey" PRIMARY KEY ("id")
+);
