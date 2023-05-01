606f91277dc1a08afe8ed20f766dace6888fc513
Damsbot64 <damien64durand@gmail.com>
2023-05-01 22:24:57 +0200

    form for add wine

--- src/pages/wines/add/index.tsx
+++ src/pages/wines/add/index.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import WineForm from "~/components/form/wineForm";
 
 function addWine() {
   return (
     <div className="xy-center flexcol gap-5 p-3">
       <h1>Add a wine</h1>
+      <WineForm />
     </div>
   );
 }
