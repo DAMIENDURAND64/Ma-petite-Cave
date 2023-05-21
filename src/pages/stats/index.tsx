16dbd5e00fb2362fa93cac904e57b1eff82afb36
Damsbot64 <damien64durand@gmail.com>
2023-05-21 22:24:44 +0200

    wip

--- src/pages/stats/index.tsx
+++ src/pages/stats/index.tsx
@@ -1,6 +1,7 @@
 import { useMantineTheme } from "@mantine/core";
 import { useSession } from "next-auth/react";
 import React from "react";
+import { DoughnutForFormatOfBottles } from "~/components/charts/DoughnutForFormatBottle";
 import { DoughnutForNumOfBottles } from "~/components/charts/DoughnutForNumOfBottles";
 import { DoughnutForValOfBottles } from "~/components/charts/DoughnutForValOfBottles";
 import Unauthorized from "~/components/unauthorized/Unauthorized";
@@ -16,6 +17,18 @@ const Index = () => {
     "Spiritueux",
     "Liqueurs",
   ];
+  const colorClassMap = [
+    "bg-red-500",
+    "bg-gray-500",
+    "bg-pink-500",
+    "bg-yellow-500",
+    "bg-brown-500",
+    "bg-green-500",
+    "bg-blue-500",
+    "bg-orange-500",
+    "bg-purple-500",
+    "bg-black",
+  ];
 
   const { data: sessionData } = useSession();
   const theme = useMantineTheme();
@@ -25,6 +38,8 @@ const Index = () => {
   }
 
   const { data: allWinesData } = api.wines.getAll.useQuery();
+  const { data: allFormatsData } = api.bottleFormat.getAll.useQuery();
+
   const wineBottlesByColor = [1, 2, 3, 4, 5, 6].reduce(
     // 1:red, 2:white, 3:rose, 4:sparkling, 5:liquors, 6:spirits
     (acc: Record<number, number>, colorId) => {
@@ -73,9 +88,26 @@ const Index = () => {
     0
   );
 
-  console.log(allWinesData);
+  const formatNames: Map<number, string> = new Map(
+    allFormatsData?.map((format) => [format.id, format.name])
+  );
+  const formatCounts = {} as Record<string, number>;
+
+  allWinesData?.forEach((wine) => {
+    wine.wineBottles.forEach((bottle) => {
+      const formatName: string = formatNames.get(bottle.formatId) || "Unknown";
+      const quantity = bottle.quantity;
+
+      if (formatCounts[formatName]) {
+        formatCounts[formatName] += quantity;
+      } else {
+        formatCounts[formatName] = quantity;
+      }
+    });
+  });
+
   return (
-    <div className="flexcol y-center w-full border border-green-200">
+    <div className="flexcol y-center w-full pb-3">
       <h1>Mes Stats</h1>
       <div className="flexcol w-full gap-3">
         <div
@@ -95,7 +127,7 @@ const Index = () => {
             {Object.entries(Colors).map(([key, color], index) => {
               return (
                 <div className="flexcol xy-center" key={index}>
-                  <div className={`${color} relative h-3 w-3 rounded-lg`} />
+                  <div className={`${color} relative h-5 w-5 rounded-lg`} />
                   <p className="text-xs">{labels[parseInt(key) - 1]}</p>
                 </div>
               );
@@ -119,7 +151,7 @@ const Index = () => {
             {Object.entries(Colors).map(([key, color], index) => {
               return (
                 <div className="flexcol xy-center" key={index}>
-                  <div className={`${color} relative h-3 w-3 rounded-lg`} />
+                  <div className={`${color} relative h-5 w-5 rounded-lg`} />
                   <p className="text-xs">{labels[parseInt(key) - 1]}</p>
                 </div>
               );
@@ -137,17 +169,24 @@ const Index = () => {
               Ajoute un vin pour voir tes stats
             </div>
           ) : (
-            <DoughnutForValOfBottles wineBottlesValues={wineBottlesValues} />
+            <DoughnutForFormatOfBottles formatCounts={formatCounts} />
           )}
-          <div className="flex w-full justify-around gap-3">
-            {Object.entries(Colors).map(([key, color], index) => {
-              return (
-                <div className="flexcol xy-center" key={index}>
-                  <div className={`${color} relative h-3 w-3 rounded-lg`} />
-                  <p className="text-xs">{labels[parseInt(key) - 1]}</p>
-                </div>
-              );
-            })}
+          <div className="flex w-full flex-wrap justify-around gap-3">
+            {formatCounts &&
+              Object.entries(formatCounts).map(([key], index) => {
+                return (
+                  <div className="flexcol xy-center" key={index}>
+                    <div className="flexcol xy-center" key={index}>
+                      <div
+                        className={`${
+                          colorClassMap[index] as string
+                        } relative h-5 w-5 rounded-lg`}
+                      />
+                    </div>
+                    <p className="text-xs">{key}</p>
+                  </div>
+                );
+              })}
           </div>
         </div>
       </div>
