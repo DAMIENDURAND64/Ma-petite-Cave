82a28790c4a63d3b5d60488e17905701de24b34d
DAMIENDURAND64 <damien64durand@gmail.com>
2023-07-14 17:41:51 +0200

    fix type error

--- src/components/welcome/WelcomePage.tsx
+++ src/components/welcome/WelcomePage.tsx
@@ -1,15 +1,18 @@
 import { useSession } from "next-auth/react";
 import Image from "next/image";
 import { useRouter } from "next/router";
-import React from "react";
+import React, { useEffect } from "react";
 
 function WelcomePage() {
   const { data: sessionData } = useSession();
   const router = useRouter();
 
-  if (sessionData) {
-    return router.push("/homepage");
-  }
+  useEffect(() => {
+    if (sessionData) {
+      router.push("/homepage").catch((err) => console.log(err));
+    }
+  }, [sessionData, router]);
+
   return (
     <div className="xy-center flex-col gap-5 p-3 pt-4">
       <p className="text-center text-4xl font-semibold">
