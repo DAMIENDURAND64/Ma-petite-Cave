0bd169cd6ddbffe502e03b1f2a5d84770a20fade
DAMIENDURAND64 <damien64durand@gmail.com>
2023-08-08 23:50:39 +0200

    modal works

--- src/components/consumedWine/ModalWineConsumed.tsx
+++ src/components/consumedWine/ModalWineConsumed.tsx
@@ -1,8 +1,10 @@
+/* eslint-disable @typescript-eslint/no-misused-promises */
 import { Modal, NumberInput, Select, TextInput, Textarea } from "@mantine/core";
 import { DateInput } from "@mantine/dates";
 import { type WineBottle } from "@prisma/client";
 import React, { useState } from "react";
 import SubmitButton from "../buttons/SubmitButton";
+import { type SubmitHandler, useForm, Controller } from "react-hook-form";
 
 type Props = {
   openedWineModalConsumed: boolean;
@@ -15,12 +17,26 @@ type Props = {
   })[];
 };
 
+type FormValues = {
+  location: string;
+  format: string;
+  quantity: number;
+  date: Date;
+  with: string;
+  comment: string;
+};
+
 const ModalWineConsumed = ({
   openedWineModalConsumed,
   closeWineModalConsumed,
   wineBottles,
 }: Props) => {
   const [value, setValue] = useState<string | null>(null);
+  const { register, handleSubmit, control } = useForm<FormValues>();
+  const onSubmit: SubmitHandler<FormValues> = (data) => {
+    console.log(data);
+    console.log("hello");
+  };
 
   const bottleData = wineBottles.map((wineBottle) => ({
     value: wineBottle.format.name,
@@ -45,42 +61,70 @@ const ModalWineConsumed = ({
       }}
       title="Give us some details"
     >
-      <Select
-        data-autofocus
-        label="Select the format"
-        placeholder="Pick one format"
-        data={bottleData}
-        dropdownPosition="bottom"
-        value={value}
-        onChange={setValue}
-      />
-      <NumberInput
-        label="How many bottles did you drink ?"
-        placeholder="Pick a number"
-        min={1}
-        max={bottleToFindForMaxNumber?.quantity}
-        defaultValue={1}
-        style={{ marginTop: "20px" }}
-      />
-      <DateInput
-        label="When did you drink it ?"
-        style={{ marginTop: "20px" }}
-      />
-      <TextInput
-        label="Where did you drink it ?"
-        style={{ marginTop: "20px" }}
-      />
-      <Textarea
-        label="With who did you drink it ?"
-        style={{ marginTop: "20px" }}
-      />
-      <Textarea
-        label="What did you think about it ?"
-        style={{ marginTop: "20px" }}
-      />
-      <div className="mt-5 flex w-full justify-end">
-        <SubmitButton label="Submit" />
-      </div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Controller
+          name="format"
+          control={control}
+          render={({ field }) => (
+            <Select
+              data-autofocus
+              label="Select the format"
+              placeholder="Pick one format"
+              data={bottleData}
+              dropdownPosition="bottom"
+              onChange={(selectedValue) => {
+                field.onChange(selectedValue);
+                setValue(selectedValue);
+              }}
+            />
+          )}
+        />
+        <Controller
+          name="quantity"
+          control={control}
+          render={({ field }) => (
+            <NumberInput
+              label="How many bottles did you drink ?"
+              placeholder="Pick a number"
+              min={1}
+              max={bottleToFindForMaxNumber?.quantity}
+              defaultValue={1}
+              style={{ marginTop: "20px" }}
+              type="number"
+              {...field}
+            />
+          )}
+        />
+        <Controller
+          name="date"
+          control={control}
+          render={({ field }) => (
+            <DateInput
+              label="When did you drink it ?"
+              style={{ marginTop: "20px" }}
+              {...field}
+            />
+          )}
+        />
+        <TextInput
+          {...register("location", { required: true })}
+          label="Where did you drink it ?"
+          style={{ marginTop: "20px" }}
+        />
+        <Textarea
+          {...register("with", { required: true })}
+          label="With who did you drink it ?"
+          style={{ marginTop: "20px" }}
+        />
+        <Textarea
+          {...register("comment", { required: true })}
+          label="What did you think about it ?"
+          style={{ marginTop: "20px" }}
+        />
+        <div className="mt-5 flex w-full justify-end">
+          <SubmitButton label="Submit" />
+        </div>
+      </form>
     </Modal>
   );
 };
